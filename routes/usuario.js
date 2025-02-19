38b6d4a7f12927ea36a51742c91fd042c6b57a93
lau-ifma <lauandersoncosta@acad.ifma.edu.br>
2025-02-19 15:33:05 -0300

    update pagina principal adicionado botao cadastre-se

--- routes/usuario.js
+++ routes/usuario.js
@@ -7,7 +7,7 @@ router.get('/login', (req, res) => res.render('usuario/login'))
 router.post('/login', UsuarioController.login)
 router.get('/logout', logado, UsuarioController.logout)
 router.get('/cadastrar', (req, res) => {
-    if (req.user?.tipo == 0) {
+    if (true) { //req.user?.tipo == 0
         res.render('usuario/cadastrar')
     } else {
         res.render('errors/401')
