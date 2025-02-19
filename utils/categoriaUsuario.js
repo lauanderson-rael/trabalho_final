42c80623ef740eb77b91e98ceb57c0de1726aa93
DennisGabriel <dennisgabr745@gmail.com>
2025-02-19 11:49:43 -0300

    comment model categoriaUsuario

--- utils/categoriaUsuario.js
+++ utils/categoriaUsuario.js
@@ -1,3 +1,6 @@
+// Esse módulo serve para sanitizar a informação do tipo do usuário, para que não fique:
+// Usuário: XPTO, tipo: 0
+// fica: Usuário XPTO: tipo: Administrador
 export default {
     0: 'Administrador',
     1: 'Usuário Comum'
